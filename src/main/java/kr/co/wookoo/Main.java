f807dd2b6158213c203b2b429fbabc851f1f24e3
김도현 <nanayagoon@daum.net>
2025-04-08 04:01:00 +0900

    Fix: 디버그 함수 제거

--- src/main/java/kr/co/wookoo/Main.java
+++ src/main/java/kr/co/wookoo/Main.java
@@ -1,6 +1,9 @@
 package kr.co.wookoo;
 
 import kr.co.wookoo.http.HttpClient;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import static java.lang.System.exit;
 
@@ -13,19 +16,11 @@ public class Main {
             System.out.println("토큰이 없습니다!");
             exit(1);
         }
-//        JDABuilder.createDefault(token)
-//                .enableIntents(GatewayIntent.GUILD_MESSAGES,GatewayIntent.MESSAGE_CONTENT)
-//                .addEventListeners(new MessageReceiver(),new AutoChat())
-//                .setActivity(Activity.playing("허리수술 2000만원"))
-//                .build();
-
-//        GraphQLClient graphQLClient = new GraphQLClient();
-//
-//        System.out.println(graphQLClient.fetchTraderResetTimes());
-
-        HttpClient httpHttpClient = new HttpClient();
-
-        System.out.println(httpHttpClient.fetchTraderResetTimes());
+        JDABuilder.createDefault(token)
+                .enableIntents(GatewayIntent.GUILD_MESSAGES,GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new MessageReceiver(),new AutoChat())
+                .setActivity(Activity.playing("허리수술 2000만원"))
+                .build();
     }
 
 }
